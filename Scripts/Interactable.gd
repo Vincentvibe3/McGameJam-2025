314d830cd0aaff1b6ecb46cea6b5a896805fdb26
Vincent <vincentvibe3@gmail.com>
2025-01-11 15:05:22 -0500

    feat: add door hitboxes

--- Scripts/Interactable.gd
+++ Scripts/Interactable.gd
@@ -8,11 +8,13 @@ func _on_interact():
 	pass
 
 func _on_body_entered(body:Node2D) -> void:
+	print("entered")
 	if body is Player:
 		body.interact.connect(_on_interact)
 		label.visible = true
 
 func _on_body_exited(body:Node2D) -> void:
+	print("exited")
 	if body is Player:
 		body.interact.disconnect(_on_interact)
 		label.visible = false
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 class_name Player
 
-signal interact()
+signal interact(interact)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
