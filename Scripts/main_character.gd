4294f06d50fac6bfc534bc198abb6104b1e667ae
Vincent <vincentvibe3@gmail.com>
2025-01-11 17:09:27 -0500

    add autoload player state

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -5,9 +5,6 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const sprint_multiplier = 2
 
-var Leg_enabled = 0
-var Face_enabled = 0
-var Torso_enabled = 0
 var top_down = 0 #use to make alternate controls for topdown
 var flappy_bird = 0 #use to make alternate controls for flappy_bird
 @onready var legs: Node2D = $Legs
@@ -16,11 +13,11 @@ var flappy_bird = 0 #use to make alternate controls for flappy_bird
 var need_to_float = 0
 
 func _physics_process(delta: float) -> void:
-	if Leg_enabled:
+	if PlayerProgress.Leg_enabled:
 		legs.visible = true
-	if Face_enabled:
+	if PlayerProgress.Face_enabled:
 		face.visible = true
-	if Torso_enabled:
+	if PlayerProgress.Torso_enabled:
 		torso.visible = true
 	
 	
@@ -29,7 +26,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor() and Leg_enabled:
+	if Input.is_action_just_pressed("jump") and is_on_floor() and PlayerProgress.Leg_enabled:
 		velocity.y = JUMP_VELOCITY
 		
 		
@@ -37,7 +34,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := 0
-	if Input.is_action_pressed("sprint") and Leg_enabled:
+	if Input.is_action_pressed("sprint") and PlayerProgress.Leg_enabled:
 		direction = Input.get_axis("move_left", "move_right")
 		if direction:
 			velocity.x = direction * SPEED * sprint_multiplier
