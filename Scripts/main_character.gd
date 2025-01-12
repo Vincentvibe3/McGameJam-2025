52bc551ce887cb9b7efb0755ed140d9714471663
simblu985 <simonbluteau413@gmail.com>
2025-01-11 23:06:17 -0500

    sprite4normal

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -14,8 +14,14 @@ var flappy_bird = 0 #use to make alternate controls for flappy_bird
 var need_to_float = 0
 @onready var torso_collision: CollisionShape2D = $TorsoCollision
 @onready var legs_collision: CollisionShape2D = $LegsCollision
+@onready var ears: Node2D = $ears
+@onready var arms: Node2D = $arms
 
 func _physics_process(delta: float) -> void:
+	if not PlayerProgress.ears_enabled:
+		ears.visible = false
+	if not PlayerProgress.arms_enabled:
+		arms.visible = false
 	if not PlayerProgress.Leg_enabled:
 		legs.visible = false
 		legs_collision.disabled = true
@@ -24,6 +30,10 @@ func _physics_process(delta: float) -> void:
 	if not PlayerProgress.Torso_enabled:
 		torso.visible = false
 		torso_collision.disabled = true
+	if PlayerProgress.ears_enabled:
+		ears.visible = true
+	if PlayerProgress.arms_enabled:
+		arms.visible = true
 	if PlayerProgress.Leg_enabled:
 		legs.visible = true
 		legs_collision.disabled = false
