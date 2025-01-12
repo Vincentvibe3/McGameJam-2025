0a86e30bd9acff844b1fb5055236ac5765ad9a56
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2025-01-11 20:55:03 -0500

    update bedroom A

--- Scripts/top_down_movement.gd
+++ Scripts/top_down_movement.gd
@@ -8,40 +8,40 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-	# 	velocity += get_gravity() * delta
+    # Add the gravity.
+    # if not is_on_floor():
+    # 	velocity += get_gravity() * delta
 
-	# # Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
+    # # Handle jump.
+    # if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+    # 	velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var movementSet = false
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -SPEED
-		movementSet = true
-	elif Input.is_action_just_released("move_left"):
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_pressed("move_right"):
-		velocity.x = SPEED
-		movementSet = true
-	elif Input.is_action_just_released("move_right"):
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_pressed("move_up"):
-		velocity.y = -SPEED
-		movementSet = true
-	elif Input.is_action_just_released("move_up"):
-		velocity.x = move_toward(velocity.y, 0, SPEED)
-	if Input.is_action_pressed("move_down"):
-		velocity.y = SPEED
-		movementSet = true
-	elif Input.is_action_just_released("move_down"):
-		velocity.x = move_toward(velocity.y, 0, SPEED)
-		
-	if !movementSet:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+    # Get the input direction and handle the movement/deceleration.
+    # As good practice, you should replace UI actions with custom gameplay actions.
+    var movementSet = false
+    if Input.is_action_pressed("move_left"):
+        velocity.x = -SPEED
+        movementSet = true
+    elif Input.is_action_just_released("move_left"):
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+    if Input.is_action_pressed("move_right"):
+        velocity.x = SPEED
+        movementSet = true
+    elif Input.is_action_just_released("move_right"):
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+    if Input.is_action_pressed("move_up"):
+        velocity.y = -SPEED
+        movementSet = true
+    elif Input.is_action_just_released("move_up"):
+        velocity.x = move_toward(velocity.y, 0, SPEED)
+    if Input.is_action_pressed("move_down"):
+        velocity.y = SPEED
+        movementSet = true
+    elif Input.is_action_just_released("move_down"):
+        velocity.x = move_toward(velocity.y, 0, SPEED)
+        
+    if !movementSet:
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+        velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	move_and_slide()
+    move_and_slide()
