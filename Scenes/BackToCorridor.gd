84667b0974180eb64412feaf5033974db362d278
Vincent <vincentvibe3@gmail.com>
2025-01-11 22:32:59 -0500

    update maze teleport

--- Scenes/BackToCorridor.gd
+++ Scenes/BackToCorridor.gd
@@ -3,7 +3,8 @@ extends Area2D
 class_name BackToCorridor
 
 @export var node_name: String
+@export var scene_name: String
 
 func _on_body_entered(body:Node2D) -> void:
 	if body is Player:
-		PlayerProgress.call_deferred("move_to",node_name ,"res://Scenes/corridor.tscn")
+		PlayerProgress.call_deferred("move_to",node_name ,scene_name)
--- Scripts/door.gd
+++ Scripts/door.gd
@@ -2,6 +2,7 @@ extends Interactable
 
 @export var node_name: String
 @export var scene_name: String
+
 func _on_interact(interaction_name:String):
 	if interaction_name == "interact":
 		PlayerProgress.call_deferred("move_to",node_name ,scene_name)
